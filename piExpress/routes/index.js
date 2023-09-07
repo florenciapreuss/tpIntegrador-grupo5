e223c01904f6566ee63064da812e3985eebdf767
florenciapreuss <fpreuss@udesa.edu.ar>
2023-09-07 14:20:33 -0300

    Hice las rutas para index, registracion, login, agregarpost y miperfil. Cambie los links del header de index.ejs

--- piExpress/routes/index.js
+++ piExpress/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/registro', function(req, res, next) {
+  res.render('registracion');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.get('/agregarPost', function(req, res, next) {
+  res.render('agregarPost');
+});
+router.get('/perfil', function(req, res, next) {
+  res.render('miPerfil');
 });
 
 module.exports = router;
