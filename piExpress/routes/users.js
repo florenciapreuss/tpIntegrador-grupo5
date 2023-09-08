fe204765f54acf16863a401cb4bfaf8fec901320
florenciapreuss <fpreuss@udesa.edu.ar>
2023-09-08 18:55:00 -0300

    cambie la ruta de usersController

--- piExpress/routes/users.js
+++ piExpress/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/usuario:', function(req, res, next) {
