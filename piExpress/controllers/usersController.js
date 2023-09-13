9c2ca925ce481b26dd87f079ba51b76f2c74d845
florenciapreuss <fpreuss@udesa.edu.ar>
2023-09-13 17:43:12 -0300

    Hice resultadoBusqueda. Modifique el formulario del header, la ruta y el controlador.

--- piExpress/controllers/usersController.js
+++ piExpress/controllers/usersController.js
@@ -1,4 +1,5 @@
-const userController = {
+const datos = require("../db/data");
+const usersController = {
     detallePost: function(req, res){
         return res.render('detallePost')
     },
@@ -6,8 +7,14 @@ const userController = {
         return res.render('detalleUsuario')
     },
     resultadoBusqueda: function(req, res){
-        return res.render('resultadoBusqueda')
+        let usuarioEncontrado;
+        for (let i = 0; i < datos.usuarios.length; i++) {
+            if (datos.usuarios[i].email == req.query.busqueda) {
+                usuarioEncontrado = datos.usuarios[i];
+            }
+        }
+        return res.render('resultadoBusqueda', {usuario: usuarioEncontrado})
     }
 }
 
-module.exports = userController;
+module.exports = usersController;
--- piExpress/routes/users.js
+++ piExpress/routes/users.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/usersController')
+const usersController = require('../controllers/usersController')
 
-/* GET users listing. */
-router.get('/usuario:', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/detallePost/:id', usersController.detallePost)
 
-router.get('/detallePost/:id', userController.detallePost)
+router.get('/detalleUsuario/:id', usersController.detalleUsuario)
 
-router.get('/detalleUsuario/:id', userController.detalleUsuario)
-
-router.get('/resultadoBusqueda/:usuario', userController.resultadoBusqueda)
+router.get('/resultadoBusqueda', usersController.resultadoBusqueda)
 
 module.exports = router;
