739755364eb949ac22457f44ef53243cab0e1275
florenciapreuss <fpreuss@udesa.edu.ar>
2023-09-15 18:00:52 -0300

    corregi resultadosBusqueda

--- piExpress/controllers/usersController.js
+++ piExpress/controllers/usersController.js
@@ -8,12 +8,7 @@ const usersController = {
     },
     resultadoBusqueda: function(req, res){
         let usuarioEncontrado;
-        for (let i = 0; i < datos.usuarios.length; i++) {
-            if (datos.usuarios[i].email == req.query.busqueda) {
-                usuarioEncontrado = datos.usuarios[i];
-            }
-        }
-        return res.render('resultadoBusqueda', {usuario: usuarioEncontrado})
+        return res.render('resultadoBusqueda', {usuarios: datos.usuarios})
     }
 }
 
