1f178ebd406096b0a3c9d5c2f13ff7cb8d53c03c
florenciapreuss <fpreuss@udesa.edu.ar>
2023-11-08 11:03:47 -0300

    arranque con controlador de detalle post y el view

--- piExpress/controllers/indexController.js
+++ piExpress/controllers/indexController.js
@@ -12,9 +12,7 @@ const indexController ={
             for (let index = 0; index < resultados.length; index++) {
                 for (let j = 0; j < resultados[index].comentarios_id_posteo.length; j++) {
                     console.log(resultados[index].comentarios_id_posteo[j].id_usuario);
-                    
                 }
-                
             }
            // return res.send(resultados)
             return res.render('index', { posteos: resultados });
@@ -22,23 +20,32 @@ const indexController ={
             return res.send(error)
           });
     },
-    registro: function(req, res){
+    registro: function(req, res){ //FALTA CONTROL DE ACCESO
         return res.render('registracion')
     },
-    login: function(req, res){
+    login: function(req, res){ //FALTA CONTROL DE ACCESO
         return res.render('login')
     },
-    miPerfil: function(req, res){       /* Quremos mostrar el perfil específico de un usuario */
-        id_mi_usuario = 1;          /* Creamos la variaable id mi usuario y le damos el valor 1 */
-        let posteos_usuario= [];        /* Creamos un array vacío para qeu se almacenen los posteos de este usuario */
-        for (let i = 0; i < datos.posteos.length; i++) {
-            if (datos.posteos[i].id_usuario == id_mi_usuario) { /* Recorre los id de los usuarios en la base de datos y cuando uno coincide con el de la iteración se pushean esos posteos en el array */
-                posteos_usuario.push(datos.posteos[i]);
+    miPerfil: function(req, res){ //FALTA CONTROL DE ACCESO Y VINCULAR AL USUARIO QUE SE LOGUEO CON ID_MI_USUARIO.
+        datos.Posteo.findAll({
+            include: [
+                {association: "usuarios_id_posteo"},
+                {association: "comentarios_id_posteo", include: {association:"usuarios_id_comentario"}}
+            ]
+        }).then((resultados) => {
+            id_mi_usuario = 1;
+            let posteos_usuario= [];
+            for (let i = 0; i < resultados.length; i++) {
+                if (resultados[i].id_usuario == id_mi_usuario) { 
+                    posteos_usuario.push(resultados[i]);
+                }
             }
-        }
-        return res.render('miPerfil', {usuario: datos.usuarios[0], posteos_usuario: posteos_usuario}) /*  renderizamos la vista mi perfil con el objeto de datos que contiene la información ylos posteos de ese usuario */
+            return res.render('miPerfil', {usuario:resultados[0], posteos_usuario: posteos_usuario})
+          }).catch(function(error) {
+            return res.send(error)
+          });
     },
-    editarPerfil: function(req, res){
+    editarPerfil: function(req, res){ //FALTA CONTROL DE ACCESO
         return res.render('editarPerfil')
     }
 }
--- piExpress/controllers/productsController.js
+++ piExpress/controllers/productsController.js
@@ -1,24 +1,21 @@
 const datos = require("../database/models");
 const productsController = {
-agregarPost: function(req, res){
+agregarPost: function(req, res){ //FALTA HACER QUE SE ENVIEN ESTOS DATOS A LA BASE DE DATOS
     return res.render('agregarPost')
 },
-detallePost: function(req, res){ /* Método con función anonima que tiene req y res como parametros */
-        let id_posteo = req.params.id; /* Con la propiedad.params traemos el id de la url del posteo que tocó el usuario */
-        let posteo ="";
-        for (let i = 0; i < datos.posteos.length; i++) { /* Encontrar el posteo que coincide con el id */
-            if (id_posteo == datos.posteos[i].id_posteo) {
-                posteo = datos.posteos[i]; /* añade el posteo que coincide con el id del posteo  */
-            }
-        }
-        let usuario_posteo ="";
-        for (let j = 0; j < datos.usuarios.length; j++) { /* Para tener la foto de perfil */
-            if (posteo.id_usuario == datos.usuarios[j].id_usuario) { /* si el id de esta renderizacion coincide con un id en posteo, mostrar la foto de perfil de ese usuario */
-                usuario_posteo = datos.usuarios[j]
-           }
-            
-        }
-        return res.render('detallePost', {posteo: posteo, usuario_posteo: usuario_posteo, usuarios: datos.usuarios}) /* Renderizamos la vista detallepost con el objeto de datos que tiene la información del posteo y del usuario que hizo el posteo */
+detallePost: function(req, res){ 
+    let id_posteo = req.params.id; 
+     datos.Posteo.findByPK(id_posteo)//{
+    //     include: [
+    //         {association: "usuarios_id_posteo"},
+    //         {association: "comentarios_id_posteo", include: {association:"usuarios_id_comentario"}}
+    //     ]
+    // })
+      .then((resultados) => {
+        return res.render('detallePost', {posteo: resultados}) 
+      }).catch(function(error) {
+        return res.send(error)
+      });
     },
 }
 module.exports = productsController;
